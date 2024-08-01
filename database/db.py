5406da92a58c8c35ac9d33b7b32745e7f8faaa1b
RK-Baovq <Baovq@rikkeisoft.com>
2024-08-01 14:45:22 +0700

    base code fix

--- database/db.py
+++ database/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:baott12345@localhost:3306/project_sales"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:baott12345@localhost:3306/library_management"
 
 metadata = sqlalchemy.MetaData()
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
